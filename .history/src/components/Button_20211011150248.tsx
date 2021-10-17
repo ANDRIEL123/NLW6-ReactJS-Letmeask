0cc3b5e9567859fdde360f11a81e3744f58707b6
andriel123 <andrielmfriedrich@gmail.com>
2021-10-17 20:57:56 -0300

    Remove history

--- .history/src/components/Button_20211011150248.tsx
+++
@@ -1,7 +0,0 @@
-export function Button(props) {
-    return (
-        <button className="button">
-
-        </button>
-    )
-}
--- .history/src/pages/Home_20211011090208.tsx
+++
@@ -1,7 +0,0 @@
-export function Home() {
-    return (
-        <div>
-            <h3>Hello World</h3>
-        </div>
-    )
-}
